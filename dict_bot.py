fae420a4b4fa9e8ab5c7ac028143e6235f8bae48
Karina-Art <131808372+KarinaArtemieva@users.noreply.github.com>
2023-05-21 21:02:14 +0300

    Update dict_bot.py

--- dict_bot.py
+++ dict_bot.py
@@ -5,7 +5,7 @@
 
 from telebot import TeleBot, types
 
-bot = TeleBot(token='5438434678:AAGpntqOQHl6hBv9htsezihpuuWtnwiZidU', parse_mode='html') # создание бота
+bot = TeleBot(token='', parse_mode='html') # создание бота
 
 # словарь с определениями и аббревиатурами, которые знает бот
 # в формате:
